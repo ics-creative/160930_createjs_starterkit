5a28670eb17f00ce761ec1bd2464df3c8147f4ac
ics-nohara <nozomi.nohara@ics-web.jp>
2016-09-28 22:46:37 +0900

    Add assets

---
+++ assets/assets-main.d.ts
@@ -0,0 +1,60 @@
+declare module lib {
+
+	export class properties implements Object {
+		static width: number;
+		static height: number;
+		static fps: number;
+		static color: string;
+		static manifest: Object[];
+	}
+
+	export var ssMetadata = Object[]
+
+	export class frame0001 extends createjs.Sprite {
+	}
+
+	export class frame0002 extends createjs.Sprite {
+	}
+
+	export class frame0003 extends createjs.Sprite {
+	}
+
+	export class frame0004 extends createjs.Sprite {
+	}
+
+	export class frame0005 extends createjs.Sprite {
+	}
+
+	export class frame0006 extends createjs.Sprite {
+	}
+
+	export class frame0007 extends createjs.Sprite {
+	}
+
+	export class frame0008 extends createjs.Sprite {
+	}
+
+	export class MyAsset2 extends createjs.MovieClip {
+		nominalBounds: createjs.Rectangle;
+		text: createjs.Text;
+		shape: createjs.Shape;
+	}
+
+	export class MyAsset1 extends createjs.MovieClip {
+		nominalBounds: createjs.Rectangle;
+		instance: frame0001;
+		instance_1: frame0002;
+		instance_2: frame0003;
+		instance_3: frame0004;
+		instance_4: frame0005;
+		instance_5: frame0006;
+		instance_6: frame0007;
+		instance_7: frame0008;
+	}
+
+	export class assetsmain extends createjs.MovieClip {
+		nominalBounds: createjs.Rectangle;
+		instance: MyAsset1;
+	}
+
+}
---
+++ assets/assets-main.js
@@ -0,0 +1,178 @@
+(function (lib, img, cjs, ss) {
+
+var p; // shortcut to reference prototypes
+
+// library properties:
+lib.properties = {
+	width: 550,
+	height: 400,
+	fps: 24,
+	color: "#FFFFFF",
+	opacity: 1.00,
+	manifest: [
+		{src:"images/assets_main_atlas_.png?1475069264904", id:"assets_main_atlas_"}
+	]
+};
+
+
+
+lib.ssMetadata = [
+		{name:"assets_main_atlas_", frames: [[0,322,320,320],[322,0,320,320],[322,322,320,320],[0,644,320,320],[644,322,320,320],[322,644,320,320],[644,0,320,320],[0,0,320,320]]}
+];
+
+
+// symbols:
+
+
+
+(lib.frame0001 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(0);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0002 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(1);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0003 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(2);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0004 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(3);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0005 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(4);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0006 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(5);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0007 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(6);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.frame0008 = function() {
+	this.spriteSheet = ss["assets_main_atlas_"];
+	this.gotoAndStop(7);
+}).prototype = p = new cjs.Sprite();
+
+
+
+(lib.MyHamster = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー 1
+	this.instance = new lib.frame0001();
+	this.instance.parent = this;
+	this.instance.setTransform(-33,-42);
+
+	this.instance_1 = new lib.frame0002();
+	this.instance_1.parent = this;
+	this.instance_1.setTransform(-33,-42);
+
+	this.instance_2 = new lib.frame0003();
+	this.instance_2.parent = this;
+	this.instance_2.setTransform(-33,-42);
+
+	this.instance_3 = new lib.frame0004();
+	this.instance_3.parent = this;
+	this.instance_3.setTransform(-33,-42);
+
+	this.instance_4 = new lib.frame0005();
+	this.instance_4.parent = this;
+	this.instance_4.setTransform(-33,-42);
+
+	this.instance_5 = new lib.frame0006();
+	this.instance_5.parent = this;
+	this.instance_5.setTransform(-33,-42);
+
+	this.instance_6 = new lib.frame0007();
+	this.instance_6.parent = this;
+	this.instance_6.setTransform(-33,-42);
+
+	this.instance_7 = new lib.frame0008();
+	this.instance_7.parent = this;
+	this.instance_7.setTransform(-33,-42);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.instance}]}).to({state:[{t:this.instance_1}]},1).to({state:[{t:this.instance_2}]},1).to({state:[{t:this.instance_3}]},1).to({state:[{t:this.instance_4}]},1).to({state:[{t:this.instance_5}]},1).to({state:[{t:this.instance_6}]},1).to({state:[{t:this.instance_7}]},1).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-33,-42,320,320);
+
+
+(lib.MyAsset2 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー 1
+	this.text = new cjs.Text("アセット2だよー", "14px 'Osaka'", "#315479");
+	this.text.lineHeight = 20;
+	this.text.lineWidth = 100;
+	this.text.parent = this;
+	this.text.setTransform(85,157);
+
+	this.shape = new cjs.Shape();
+	this.shape.graphics.f("#FFFFFF").s().p("A1OYIMAAAgwPMAqdAAAMAAAAwPg");
+	this.shape.setTransform(136,154.5);
+
+	this.timeline.addTween(cjs.Tween.get({}).to({state:[{t:this.shape},{t:this.text}]}).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(0,0,272,309);
+
+
+(lib.MyAsset1 = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー 1
+	this.hamham = new lib.MyHamster();
+	this.hamham.parent = this;
+	this.hamham.setTransform(121.6,161.3,1,1,0,0,0,127.2,163.8);
+
+	this.timeline.addTween(cjs.Tween.get(this.hamham).to({regX:0,regY:0,x:537.7,y:-16.7},24).wait(1));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(-38.6,-44.5,320,320);
+
+
+// stage content:
+
+
+
+(lib.assetsmain = function(mode,startPosition,loop) {
+	this.initialize(mode,startPosition,loop,{});
+
+	// レイヤー 1
+	this.instance = new lib.MyAsset1();
+	this.instance.parent = this;
+	this.instance.setTransform(160,-105.9,1,1,0,0,0,160,160);
+
+	this.timeline.addTween(cjs.Tween.get(this.instance).wait(26));
+
+}).prototype = p = new cjs.MovieClip();
+p.nominalBounds = new cjs.Rectangle(236.4,-110.4,320,320);
+
+})(lib = lib||{}, images = images||{}, createjs = createjs||{}, ss = ss||{});
+var lib, images, createjs, ss;
